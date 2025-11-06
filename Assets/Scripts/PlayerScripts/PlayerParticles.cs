ff1d6f9996efa66ee0bc50dc17521fcb1b6f8504
VerhelleLuka <luka.verhelle@hotmail.com>
2025-11-06 13:32:11 +0100

    Changed some assets

--- Assets/Scripts/PlayerScripts/PlayerParticles.cs
+++ Assets/Scripts/PlayerScripts/PlayerParticles.cs
@@ -17,7 +17,11 @@ public class PlayerParticles : MonoBehaviour
     void Update()
     {
         this.transform.localRotation = Quaternion.Inverse(transform.parent.localRotation); //Keep particles from rotating with parent
-           
+         
+        if((Mathf.Abs(_PlayerMovement.body.velocity.x) < velocityThreshold || Mathf.Abs(_PlayerMovement.body.velocity.y) < velocityThreshold) && _PlayerMovement.GetCurrentStateName() == "WallRunState" && _ParticleSystem.isStopped)
+        {
+            _ParticleSystem.Play();
+        }
         if((Mathf.Abs(_PlayerMovement.body.velocity.x) < velocityThreshold || _PlayerMovement.groundCount == 0) && !_ParticleSystem.isStopped)
         {
             _ParticleSystem.Stop();
